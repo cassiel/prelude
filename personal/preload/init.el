42868ee82f5bac06726e33a9a34ce30203d5668d
Nick (Git client) <nick@cassiel.com>
2020-09-26 17:17:31 +0100

    Shortcuts.

--- personal/preload/init.el
+++ personal/preload/init.el
@@ -1,6 +1,3 @@
-;; Make org-agenda accessible:
-(global-set-key "\C-ca" 'org-agenda)
-
 (require 'paredit)
 (add-hook 'clojure-mode-hook 'paredit-mode)
 (add-hook 'clojurescript-mode-hook 'paredit-mode)
@@ -8,3 +5,10 @@
 (require 'rainbow-delimiters)
 (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'clojurescript-mode-hook 'rainbow-delimiters-mode)
+
+; Personal shortcuts:
+(global-set-key "\C-ca" 'org-agenda)
+
+(global-set-key "\C-o" 'avy-goto-char)
+(global-set-key "\M-g\M-w" 'avy-goto-word-1)
+(global-set-key "\M-g\M-l" 'avy-goto-line)
