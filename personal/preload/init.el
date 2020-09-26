c266ca99bb928adb9fe4ac526a307468b33b3809
Nick (Git client) <nick@cassiel.com>
2020-09-26 18:18:31 +0100

    Project switching.

--- personal/preload/init.el
+++ personal/preload/init.el
@@ -8,6 +8,9 @@
 
 ;; Commander when switching projects:
 (setq projectile-switch-project-action 'projectile-commander)
+(def-projectile-commander-method ?\C-?
+  "Go back to project selection."
+  (projectile-switch-project))
 
 ;; Personal shortcuts:
 (global-set-key "\C-ca" 'org-agenda)
@@ -15,4 +18,3 @@
 (global-set-key "\C-o" 'avy-goto-char)
 (global-set-key "\M-g\M-w" 'avy-goto-word-1)
 (global-set-key "\M-g\M-l" 'avy-goto-line)
-
