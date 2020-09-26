892ba351f8fd65549a5a743adc919611fa894875
Nick (Git client) <nick@cassiel.com>
2020-09-26 18:15:54 +0100

    projectile-commander.

--- personal/preload/init.el
+++ personal/preload/init.el
@@ -6,9 +6,13 @@
 (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'clojurescript-mode-hook 'rainbow-delimiters-mode)
 
-; Personal shortcuts:
+;; Commander when switching projects:
+(setq projectile-switch-project-action 'projectile-commander)
+
+;; Personal shortcuts:
 (global-set-key "\C-ca" 'org-agenda)
 
 (global-set-key "\C-o" 'avy-goto-char)
 (global-set-key "\M-g\M-w" 'avy-goto-word-1)
 (global-set-key "\M-g\M-l" 'avy-goto-line)
+
