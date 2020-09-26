d2d64f03a4315561a8237a747341d5ea68139b44
Nick (Git client) <nick@cassiel.com>
2020-09-26 17:07:38 +0100

    paredit, rainbow-delimiters.

---
+++ personal/README.md
@@ -0,0 +1,7 @@
+# NOTES
+
+Packages to install manually (probably before `personal/preload/init.el` established):
+
+- `cider`
+- `paredit`
+- `rainbow-delimiters`
--- personal/preload/init.el
+++ personal/preload/init.el
@@ -1,3 +1,10 @@
 ;; Make org-agenda accessible:
 (global-set-key "\C-ca" 'org-agenda)
 
+(require 'paredit)
+(add-hook 'clojure-mode-hook 'paredit-mode)
+(add-hook 'clojurescript-mode-hook 'paredit-mode)
+
+(require 'rainbow-delimiters)
+(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'clojurescript-mode-hook 'rainbow-delimiters-mode)
