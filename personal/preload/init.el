edfb99ae37793a206df90d89b3198648513b3634
Nick (Git client) <nick@cassiel.com>
2022-02-03 18:24:51 +0000

    Tweaks.

---
+++ personal/preload/init.el
@@ -0,0 +1,10 @@
+(require 'rainbow-delimiters)
+(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'clojurescript-mode-hook 'rainbow-delimiters-mode)
+
+;; Commander when switching projects:
+(require 'projectile)
+(setq projectile-switch-project-action 'projectile-commander)
+(def-projectile-commander-method ?\C-?
+  "Go back to project selection."
+  (projectile-switch-project))
