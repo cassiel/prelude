9a11c0e829903ef99756b512634783d84a4fb10d
Nick (Git client) <nick@cassiel.com>
2022-02-04 15:52:03 +0000

    Smartparens strict.

--- personal/preload/init.el
+++ personal/preload/init.el
@@ -2,6 +2,9 @@
 (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'clojurescript-mode-hook 'rainbow-delimiters-mode)
 
+(add-hook 'clojure-mode-hook 'turn-on-smartparens-strict-mode)
+(add-hook 'clojurescript-mode-hook 'turn-on-smartparens-strict-mode)
+
 ;; Commander when switching projects:
 (require 'projectile)
 (setq projectile-switch-project-action 'projectile-commander)
